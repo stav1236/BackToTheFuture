106a975cd4b3cc67bfa9757ea2c677a9f3e1ebef
unknown <stavmaor1@gmail.com>
2020-12-03 13:54:34 +0200

    Clock with start and stop button

--- Main.js
+++ Main.js
@@ -1,13 +1,22 @@
 let clockOn = false;
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("clock").innerHTML = new Date().toLocaleTimeString();
+});
+
+setClockTime = () => {
+    if (clockOn === true) {
+        document.getElementById("clock").innerHTML = new Date().toLocaleTimeString();
+    }
+}
+
 startCloack = () => {
     clockOn = !clockOn
-    console.log("hllo")
     if (clockOn === true) {
         document.getElementById("clockController").innerHTML = "הפסק";
-
-
     } else {
         document.getElementById("clockController").innerHTML = "הפעל";
     }
 }
+
+let clockInterval = setInterval(setClockTime, 1000);
