af70416b32e249afd1a628a980f9075ad24425fb
unknown <stavmaor1@gmail.com>
2020-12-06 17:06:27 +0200

    adding sort option and show specific details and hover and click select row

--- Main.js
+++ Main.js
@@ -1,13 +1,23 @@
 let clockOn = false;
-const youngData = [{ "Id": "1", "Name": "חבר פרבר", "Place": "פני חבר", "Phone": "058-5648444" },
-    { "Id": "48", "Name": "עדי שטיינר", "Place": "להבים", "Phone": "051-1234567" },
-    { "Id": "99", "Name": "סאני סימן-טוב", "Place": "חולון", "Phone": "012-1234567" }
+const youngData = [{ "Id": 1, "Name": "חבר פרבר", "Place": "פני חבר", "Phone": "058-5648444", "SpecificDeatils": { "Hobit": "קולנוע", "Book": 'מנהרת הזמן' } },
+    { "Id": 48, "Name": "עדי שטיינר", "Place": "להבים", "Phone": "051-1234567", "SpecificDeatils": { "Hobit": "בילויים", "Book": 'חדווא"2' } },
+    { "Id": 99, "Name": "סאני סימן-טוב", "Place": "חולון", "Phone": "012-1234567", "SpecificDeatils": { "Hobit": "בילויים", "Book": 'הארי פוטר' } }
 ]
 
 window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("clock").innerHTML = new Date().toLocaleTimeString();
     document.getElementById("allDetailsTable").style.display = "none";
+    document.getElementById("specificDetailsTable").style.display = "none";
+    loadAllDetailsTable();
+});
+
+loadAllDetailsTable = () => {
     let allDetailsTable = document.getElementById("allDetailsTable");
+    let tableRows = allDetailsTable.getElementsByTagName('tr');
+    let len = tableRows.length
+    for (let row = 0; row < len - 1; row++) {
+        allDetailsTable.removeChild(tableRows[1]);
+    }
     youngData.forEach(young => {
         let row = document.createElement('tr');
         row.classList.add('defualtRowDetailsTabale')
@@ -23,26 +33,72 @@ window.addEventListener('DOMContentLoaded', (event) => {
         row.appendChild(nameCol);
         row.appendChild(placeCol);
         row.appendChild(phoneCol);
-
         allDetailsTable.appendChild(row);
     });
-});
-
-
-readYoundData = () => {
-    youngData.forEach(element => {
-        console.log(element.Id)
-    });
+    highlightRow()
 }
 
+sortTable = () => {
+    youngData.sort((a, b) => a.Id - b.Id)
+    loadAllDetailsTable()
+}
+
+sortDownTable = () => {
+    youngData.sort((a, b) => b.Id - a.Id)
+    loadAllDetailsTable()
+}
+
+showSpecificDetails = (rowIndex) => {
+    let specificDetailsTable = document.getElementById("specificDetailsTable");
+    let tableRows = specificDetailsTable.getElementsByTagName('tr');
+    let len = tableRows.length
+    for (let row = 0; row < len; row++) {
+        specificDetailsTable.removeChild(tableRows[0]);
+    }
+
+    const youngName = youngData[rowIndex].Name;
+    const specificData = youngData[rowIndex].SpecificDeatils;
+
+    let row = document.createElement('tr');
+    let nameCol = document.createElement('td');
+    nameCol.innerText = 'שם:' + youngName;
+    let hobitCol = document.createElement('td');
+    hobitCol.innerText = 'תחביב:' + specificData.Hobit;
+    let bookCol = document.createElement('td');
+    bookCol.innerText = 'ספר:' + specificData.Book;
+    row.appendChild(nameCol);
+    row.appendChild(hobitCol);
+    row.appendChild(bookCol);
+    specificDetailsTable.appendChild(row);
+}
+
+highlightRow = () => {
+    let allDetailsTable = document.getElementById('allDetailsTable');
+    let cells = allDetailsTable.getElementsByTagName('td');
+    for (let cellIndex = 0; cellIndex < cells.length; cellIndex++) {
+        let cell = cells[cellIndex];
+        cell.onclick = function() {
+            let rowId = this.parentNode.rowIndex;
+            let rowsNotSelected = allDetailsTable.getElementsByTagName('tr');
+            for (let row = 0; row < rowsNotSelected.length; row++) {
+                rowsNotSelected[row].style.backgroundColor = "";
+            }
+            let rowSelected = allDetailsTable.getElementsByTagName('tr')[rowId];
+            rowSelected.style.backgroundColor = "lightblue";
+            showSpecificDetails(rowId - 1)
+        }
+    }
+}
 
 loadAliensPage = () => {
     document.getElementById("allDetailsTable").style.display = "none";
+    document.getElementById("specificDetailsTable").style.display = "none";
 
 }
 
 loadYoungPage = () => {
     document.getElementById("allDetailsTable").style.display = "";
+    document.getElementById("specificDetailsTable").style.display = "";
 }
 
 setClockTime = () => {
